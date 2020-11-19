257d43c92234951b487e2dd2aab9ace8d93f7b07
Christoph Eickhoff <christoh.eickhoff@mupfel.net>
2020-11-19 13:12:34 +0100

    add missing code

--- esp/light_box/src/main.cpp
+++ esp/light_box/src/main.cpp
@@ -1,9 +1,70 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+#include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define X 150
+
+Adafruit_NeoPixel strip(23, 16, NEO_GRB + NEO_KHZ800);
+AsyncWebServer server(80);
+const char *ssid = "se-ma";
+const char *password = "sewlan#42";
+void notFound(AsyncWebServerRequest *request)
+{
+	request->send(404, "application/json", "{\"message\":\"Not found\"}");
+}
+void setColor(uint32_t ledColor)
+{
+	for (int i = 0; i < strip.numPixels(); i++)
+	{
+		strip.setPixelColor(i, ledColor);
+	}
+	strip.show();
+}
+void setupPixel()
+{
+	strip.begin();
+	strip.setBrightness(100);
+	strip.show();
+
+	setColor(strip.Color(0, 0, X));
+	delay(2000);
+	setColor(strip.Color(0, 0, 0));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+	Serial.begin(9600);
+	setupPixel();
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	while (WiFi.waitForConnectResult() != WL_CONNECTED)
+	{
+		Serial.printf("WiFi Failed!\n");
+	}
+	Serial.print("IP Address: ");
+	Serial.println(WiFi.localIP());
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send(200, "application/json", "{\"message\":\"Welcome\"}");
+	});
+
+	server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+		setColor(strip.Color(X, 0, 0));
+		request->send(200, "application/json", "{\"message\":\"ok\"}");
+	});
+
+	server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
+		setColor(strip.Color(0, 0, 0));
+		request->send(200, "application/json", "{\"message\":\"ok\"}");
+	});
+
+	server.onNotFound(notFound);
+	server.begin();
+}
+
+
+void loop()
+{
 }
